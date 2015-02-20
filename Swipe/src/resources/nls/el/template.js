061b914a75e0d9d59a020cc60fac14e027c155d7
Mark Cooney <mcooney@esri.com>
2015-02-20 11:39:41 -0800

    1.4.4

---
+++ Swipe/src/resources/nls/el/template.js
@@ -0,0 +1,280 @@
+﻿define(
+	 ({
+		viewer: {
+			loading: {
+				step1: "ΦΟΡΤΩΣΗ ΕΦΑΡΜΟΓΗΣ",
+				step2: "ΦΟΡΤΩΣΗ ΔΕΔΟΜΕΝΩΝ",
+				step3: "ΑΡΧΙΚΟΠΟΙΗΣΗ",
+				fail: "Λυπούμαστε, η φόρτωση του Swipe προτύπου απέτυχε",
+				loadBuilder: "ΜΕΤΑΒΑΣΗ ΣΤΗ ΛΕΙΤΟΥΡΓΙΑ ΕΡΓΑΛΕΙΟΥ ΔΗΜΙΟΥΡΓΙΑΣ",				
+				redirectSignIn: "ΑΝΑΚΑΤΕΥΘΥΝΣΗ ΣΤΗ ΣΕΛΙΔΑ ΕΙΣΟΔΟΥ",
+				redirectSignIn2: "(θα ανακατευθυνθείτε εδώ μετά την είσοδο)",
+				failButton: "Επανάληψη"
+			},
+			errors: {
+				boxTitle: "Παρουσιάστηκε σφάλμα",
+				portalSelf: "Ανεπανόρθωτο σφάλμα: η λήψη της διαμόρφωσης της πύλης απέτυχε",
+				invalidConfig: "Ανεπανόρθωτο σφάλμα: μη έγκυρη διαμόρφωση",
+				invalidConfigNoWebmap: "Ανεπανόρθωτο σφάλμα: μη έγκυρη διαμόρφωση (δεν έχει καθοριστεί web χάρτης)",
+				createMap: "Δεν είναι δυνατή η δημιουργία χάρτη",
+				invalidApp: "Ανεπανόρθωτο σφάλμα: δεν είναι δυνατή η φόρτωση της εφαρμογής",
+				initMobile: "Καλώς ορίσατε στην web εφαρμογή Swipe. Η εφαρμογή δεν έχει διαμορφωθεί. Το διαδραστικό εργαλείο δημιουργίας δεν υποστηρίζεται σε φορητές συσκευές.",
+				noBuilderIE8: "Το διαδραστικό εργαλείο δημιουργίας Swipe δεν υποστηρίζεται στον Internet Explorer πριν από την έκδοση 9.",
+				noLayerView: "Καλώς ορίσατε στην web  εφαρμογή Swipe.<br />Η εφαρμογή δεν έχει διαμορφωθεί ακόμη.",
+				appSave: "Σφάλμα κατά την αποθήκευση της web εφαρμογής",
+				mapSave: "Σφάλμα κατά την αποθήκευση του web χάρτη",
+				notAuthorized: "Δεν έχετε εξουσιοδότηση για πρόσβαση σε αυτή την εφαρμογή",
+				conflictingProjectionsTitle: "Αλληλοσυγκρουόμενες προβολές",
+				conflictingProjections: "To Swipe δεν υποστηρίζει τη χρήση δύο web χαρτών με διαφορετικές προβολές. Ανοίξτε τις ρυθμίσεις και χρησιμοποιήστε έναν web χάρτη που χρησιμοποιεί την ίδια προβολή με τον πρώτο web χάρτη.",
+				cpButton: "Κλείσιμο"
+			},
+			mobileView: {
+				hideIntro: "ΑΠΟΚΡΥΨΗ ΕΙΣΑΓΩΓΗΣ",
+				navLeft: "Υπόμνημα",
+				navMap: "Χάρτης",
+				navRight: "Δεδομένα"
+			},
+			desktopView: {
+				storymapsText: "Ένα story map",
+				builderButton: "Μετάβαση στη λειτουργία εργαλείου δημιουργίας",
+				bitlyTooltip: "Λήψη ενός σύντομου συνδέσμου της εφαρμογή"
+			}
+		},
+		builder: {
+			builder: {
+				panelHeader: "ΔΙΑΜΟΡΦΩΣΗ ΕΦΑΡΜΟΓΗΣ",
+				buttonSave: "ΑΠΟΘΗΚΕΥΣΗ",
+				buttonHelp: "Βοήθεια",
+				buttonShare: "Κοινοποίηση",
+				buttonDiscard: "ΑΚΥΡΟ",
+				buttonSettings: "Ρυθμίσεις",
+				buttonView: "Λειτουργία προβολής",
+				buttonItem: "Άνοιγμα της web εφαρμογής",
+				noPendingChange: "Δεν υπάρχει εκκρεμής αλλαγή",
+				unSavedChangeSingular: "1 μη αποθηκευμένη αλλαγή",
+				unSavedChangePlural: "μη αποθηκευμένες αλλαγές",
+				popoverDiscard: "Είστε βέβαιοι ότι θέλετε να απορρίψετε τυχόν μη αποθηκευμένες αλλαγές;",
+				yes: "Ναι",
+				no: "Όχι",
+				popoverOpenViewExplain: "Ανοίγοντας το εργαλείο προβολής, θα χάσετε τυχόν μη αποθηκευμένες αλλαγές",
+				popoverOpenViewOk: "OK",
+				popoverOpenViewCancel: "Άκυρο",
+				popoverSaveWhenDone: "Μην ξεχάσετε να αποθηκεύσετε όταν τελειώσετε",
+				closeWithPendingChange: "Είστε βέβαιοι ότι θέλετε να επιβεβαιώσετε την ενέργεια; Οι αλλαγές σας θα χαθούν.",
+				gotIt: "OK",
+				savingApplication: "Αποθήκευση εφαρμογής",
+				saveSuccess: "Η εφαρμογή αποθηκεύτηκε με επιτυχία",
+				saveError: "Η αποθήκευση απέτυχε, δοκιμάστε ξανά",
+				saveError2: "Η αποθήκευση απέτυχε λόγω μη έγκυρης HTML ετικέτας σε ένα όνομα ή μια περιγραφή",
+				saveError3: "Ο τίτλος δεν μπορεί να είναι κενός",
+				signIn: "Εισέλθετε με έναν λογαριασμό στο",
+				signInTwo: "για να αποθηκεύσετε την εφαρμογή."
+			},
+			header:{
+				editMe: "Επεξεργαστείτε!",
+				templateTitle: "Ορισμός τίτλου προτύπου",
+				templateSubtitle: "Ορισμός υπότιτλου προτύπου"
+			},
+			settings: {
+				settingsHeader: "Ρυθμίσεις εφαρμογής",
+				modalCancel: "Άκυρο",
+				modalApply: "Εφαρμογή"
+			},
+			settingsColors: {
+				settingsTabColor: "Θέμα",
+				settingsColorExplain: "Επιλέξτε ένα θέμα για την εφαρμογή ή ορίστε τα δικά σας χρώματα.",
+				settingsLabelColor: "Χρώματα φόντου κεφαλίδας και πλαϊνού πλαισίου"
+			},
+			settingsHeader: {
+				settingsTabLogo: "Κεφαλίδα",
+				settingsLogoExplain: "Προσαρμόστε το λογότυπο της κεφαλίδας (το μέγιστο μέγεθος είναι 250 x 50 px).",
+				settingsLogoEsri: "Λογότυπο Esri",
+				settingsLogoNone: "Χωρίς λογότυπο",
+				settingsLogoCustom: "Προσαρμοσμένο λογότυπο",
+				settingsLogoCustomPlaceholder: "URL εικόνας",
+				settingsLogoCustomTargetPlaceholder: "Ενσωματωμένος σύνδεσμος",
+				settingsLogoSocialExplain: "Προσαρμόστε τον πάνω δεξιό σύνδεσμο της κεφαλίδας.",
+				settingsLogoSocialText: "Κείμενο",
+				settingsLogoSocialLink: "Σύνδεσμος",
+				settingsLogoSocialDisabled: "Αυτή η λειτουργία έχει απενεργοποιηθεί από το Διαχειριστή"
+			},
+			settingsExtent: {
+				settingsTabExtent: "Έκταση",
+				settingsExtentExplain: "Ορίστε την αρχική έκταση στον διαδραστικού χάρτη παρακάτω.",
+				settingsExtentExplainBottom: "Η έκταση που καθορίζετε θα τροποποιήσει την αρχική έκταση του web χάρτη σας. Σημειώστε ότι εάν δημιουργείτε μια σειρά σαρώσεων, η έκταση αυτή δεν θα χρησιμοποιηθεί.",
+				settingsExtentDateLineError: "Η έκταση δεν μπορεί να τέμνει τον μεσημβρινό γεωγραφικού μήκους 180ï¿½",
+				settingsExtentDateLineError2: "Σφάλμα κατά τον υπολογισμό της έκτασης",
+				settingsExtentDrawBtn: "Σχεδίαση νέας έκτασης",
+				settingsExtentModifyBtn: "Επεξεργασία της τρέχουσας έκτασης",
+				settingsExtentApplyBtn: "Εφαρμογή στον κύριο χάρτη",
+				settingsExtentUseMainMap: "Χρήση έκτασης κύριου χάρτη"
+			}
+        },
+		swipe: {
+			mobileData: {
+				noData: "Δεν υπάρχουν δεδομένα για εμφάνιση!",
+				noDataExplain: "Πατήστε στο χάρτη για να επιλέξετε ένα στοιχείο και επιστρέψτε εδώ",
+				noDataMap: "Δεν υπάρχουν δεδομένα για αυτόν το χάρτη",
+				noPopup: "Δεν βρέθηκε αναδυόμενο παράθυρο για αυτό το στοιχείο"
+			},
+			mobileLegend: {
+				noLegend: "Δεν υπάρχει υπόμνημα για εμφάνιση."
+			},
+			swipeSidePanel: {
+				editTooltip: "Ορισμός της περιγραφής του πλαϊνού πλαισίου",
+				editMe: "Επεξεργαστείτε!",
+				legendTitle: "Υπόμνημα"
+			},
+			infoWindow: {
+				noFeature: "Δεν υπάρχουν δεδομένα για εμφάνιση",
+				noFeatureExplain: "Πατήστε στο χάρτη για να επιλέξετε ένα στοχείο"
+			},
+			settingsLayout: {
+				settingsTabLayout: "Στυλ σάρωσης",
+				settingsLayoutExplain: "Επιλέξτε ένα στυλ για το εργαλείο Swipe.",
+				settingsLayoutSwipe: "Κατακόρυφη μπάρα",
+				settingsLayoutSpyGlass: "Κιάλι",
+				settingsLayoutSelected: "Επιλεγμένη διάταξη",
+				settingsLayoutSelect: "Επιλογή αυτής της διάταξης",
+				settingsSaveConfirm: "Για ορισμένες από τις αλλαγές σας απαιτείται να αποθηκεύσετε και να φορτώσετε ξανά την εφαρμογή"
+			},
+			settingsDataModel: {
+				settingsTabDataModel: "Τύπος σάρωσης",
+				settingsDataModelExplainSwipe: "Τι θέλετε να σαρώνουν οι χρήστες;",
+				settingsDataModelExplainSwipe2: "",
+				settingsDataModelExplainSpyGlass: "Επιλέξτε το θεματικό επίπεδο ή το web χάρτη που θα εμφανίζεται στο spyglass.",
+				settingsDataModelOneMap: "Ένα θεματικό επίπεδο σε έναν web χάρτη",
+				settingsDataModel1Explain: "Επιλέξτε το θεματικό επίπεδο που θέλετε να σαρώνετε",
+				settingsDataModel1Warning: "Εάν το θεματικό επίπεδο κρύβεται από ανώτερα επίπεδα, τότε η σάρωση δεν θα έχει κανένα αποτέλεσμα.",
+				settingsDataModel1SpyGlassExplain: "Επιλέξτε το θεματικό επίπεδο που θα εμφανίζεται μέσα στο spyglass.",
+				settingsDataModelTwoMaps: "Δύο web χάρτες",
+				settingsDataModelLayerIds: "ID θεματικών επιπέδων web χάρτη",
+				settingsDataModelSelected: "Επιλεγμένος τύπος",
+				settingsDataModelWebmapSwipeId1: "ID δεξιού web χάρτη",
+				settingsDataModelWebmapSwipeId2: "ID αριστερού web χάρτη",
+				settingsDataModelWebmapGlassId1: "ID κύριου web χάρτη",
+				settingsDataModelWebmapGlassId2: "ID web χάρτη Spyglass",
+				settingsDataModelSelect: "Επιλογή αυτού του τύπου",
+				settingsDataModel2Explain: "Σάρωση με άλλον web χάρτη.",
+				settingsDataModel2SpyGlassExplain: "Αποκάλυψη άλλου web χάρτη.",
+				settingsDataModel2HelpTitle: "Πώς μπορώ να βρω το ID ενός web χάρτη;",
+				settingsDataModel2HelpContent: "Αντιγράψτε και επικολλήστε τα ψηφία μετά το σύμβολο '=' στο URL του web χάρτη",
+				switchMaps: "Αλλαγή χαρτών",
+				browseWebMaps: "Αναζήτηση web χαρτών"
+			},
+			settingsLegend: {
+				settingsTabLegend: "Διάταξη εφαρμογής",
+				settingsLegendExplain: "Επιλέξτε τις ρυθμίσεις διάταξης της εφαρμογής.",
+				settingsLegendEnable: "Ενεργοποίηση υπομνήματος",
+				settingsDescriptionEnable: "Ενεργοποίηση περιγραφής",
+				settingsBookmarksEnable: "Ενεργοποίηση σειράς σαρώσεων",
+				settingsPopupDisable: "Ενεργοποίηση αναδυόμενου παραθύρου",
+				settingsLocationSearchEnable: "Ενεργοποίηση αναζήτησης τοποθεσίας",
+				settingsGeolocatorEnable: "Ενεργοποίηση γωγραφικής τοποθέτησης",
+				settingsLegendHelpContent: "Για να τελειοποιήσετε το περιεχόμενο του υπομνήματος, χρησιμοποιήστε τον πίνακα περιεχομένων του εργαλείου προβολής  web χαρτών του ArcGIS.com (Απόκρυψη στο υπόμνημα)",
+				settingsSeriesHelpContent: "Η σειρά σαρώσεων είναι μια επιλογή πλοήγησης με καρτέλες η οποία κατευθύνει το χρήστη σε μια συγκεκριμένη έκταση και εμφανίζει έναν τίτλο και μια περιγραφή στο πλευρικό πλαίσιο. Κατά την αρχική ενεργοποίηση, οι σελιδοδείκτες από τον ή τους web χάρτες θα εισαχθούν και θα χρησιμοποιηθούν για τη συμπλήρωση της μπάρας της σειράς. Με την απενεργοποίηση της επιλογής σειράς απενεργοποιείται η μπάρα της σειράς, αλλά η διαμόρφωση της σειράς διατηρείται για μελλοντική χρήση.", 
+				settingsSeriesHelpContent2: "Η σειρά σαρώσεων σας επιτρέπει να δημιουργήσετε και να επεξεργαστείτε μια επιλογή τοποθεσιών με συνοδευτικούς τίτλους και κείμενο. Εάν ο web χάρτης σας έχει σελιδοδείκτες, θα εμφανίζονται. Μπορείτε να απενεργοποιήσετε τη σειρά, αλλά η διαμόρφωση θα διατηρηθεί για μελλοντική χρήση.",
+				settingsSeriesHelpLink: "Δείτε ένα παράδειγμα εφαρμογής με μια σειρά σαρώσεων εδώ",
+				preview: "Προεπισκόπηση περιβάλλοντος εργασίας (UI)",
+				settingsLocateButtonExplain: "Αυτή η λειτουργία υποστηρίζεται στα περισσότερα προγράμματα περιήγησης για φορητές συσκευές και επιτραπέζιους υπολογιστές (συμπεριλαμβανομένου του Internet Explorer 9+).",
+				settingsLocateButton: "Ενεργοποίηση ενός κουμπιού 'Εντοπισμός' σε υποστηριζόμενα προγράμματα περιήγησης",
+				settingsAddressSearch: "Ενεργοποίηση εργαλείου αναζήτησης διεύθυνσης"
+			},
+			settingsSwipePopup: {
+				settingsSwipePopup: "Αναδυόμενο παράθυρο",
+				settingsSwipePopupExplain: "Προσαρμόστε την εμφάνιση της κεφαλίδας του αναδυόμενου παραθύρου για να βοηθήσετε το χρήστη να συσχετίσει τα αναδυόμενα παράθυρα με τα θεματικά επίπεδα του χάρτη.",
+				settingsSwipePopupSwipe1: "Αριστερός χάρτης",
+				settingsSwipePopupSwipe2: "Δεξιός χάρτης",
+				settingsSwipePopupGlass1: "Κύριος χάρτης",
+				settingsSwipePopupGlass2: "Spyglass χάρτης",
+				settingsSwipePopupTitle: "Τίτλος κεφαλίδας",
+				settingsSwipePopupColor: "Χρώμα κεφαλίδας"
+			},
+			initPopup: {
+				initHeader: "Καλώς ορίσατε στο εργαλείο δημιουργίας  Swipe/Spyglass",
+				modalNext: "Επόμενο",
+				modalPrev: "Προηγούμενο",
+				modalApply: "Άνοιγμα της εφαρμογής"
+			},
+			seriesPanel: {
+				title: "Τίτλος",
+				descr: "Περιγραφή",
+				discard: "Απόρριψη σελιδοδείκτη",
+				saveExtent: "Ορισμός έκτασης σελιδοδείκτη",
+				discardDisabled: "Δεν μπορείτε να καταργήσετε αυτόν το σελιδοδείκτη. Η σειρά σαρώσεων μπορεί να απενεργοποιηθεί στις ρυθμίσεις."
+			},
+			helpPopup: {
+				title: "Βοήθεια",
+				close: "Κλείσιμο",
+				tab1: {
+					div1: "Το Swipe/Spyglass πρότυπο είναι σχεδιασμένο για τη σύγκριση δύο ξεχωριστών web χαρτών ή δύο θεματικών επιπέδων ενός web χάρτη σε μια ελκυστική και εύχρηστη web εφαρμογή που μπορεί να χρησιμοποιηθεί σε οποιοδήποτε πρόγραμμα περιήγησης σε οποιαδήποτε συσκευή, συμπεριλαμβανομένων των smartphone και των tablet.",
+					div2: "Για πρόσθετες πληροφορίες σχετικά με το Swipe/Spyglass πρότυπο, καθώς και παραδείγματα που έχουν δημιουργηθεί από χρήστες, <a href='http://storymaps.arcgis.com/el/app-list/swipe/' target='_blank'>επισκεφθείτε τον ιστότοπο Story Maps</a>. Μπορείτε επίσης να μας ακολουθήσετε στο Twitter στο <a href='https://twitter.com/EsriStoryMaps' target='_blank'>@EsriStoryMaps</a>.",
+					div3: "Θέλουμε πολύ να ακούσουμε τα σχόλιά σας! Εάν έχετε μια ερώτηση, θέλετε να ζητήσετε μια νέα δυνατότητα ή πιστεύετε ότι βρήκατε κάποιο σφάλμα, επισκεφθείτε το <a href='http://links.esri.com/storymaps/forum' target='_blank'>Φόρουμ χρηστών του Story Maps</a>."
+				}
+			},
+			share: {
+				firstSaveTitle: "Η εφαρμογή αποθηκεύτηκε με επιτυχία",
+				firstSaveHeader: "Η εφαρμογή σας είναι τώρα αποθηκευμένη στο ArcGIS Online. Διαβάστε τις παρακάτω απαντήσεις σε συχνές ερωτήσεις.",
+				firstSaveA1: "Εάν δεν είστε εξοικειωμένοι με το ArcGIS Online ή θέλετε μια συντόμευση για πρόσβαση στο περιβάλλον διαμόρφωσης, μπορείτε να αποθηκεύσετε το σύνδεσμο %LINK1%",
+				firstSaveA1bis: "Μπορείτε επίσης να βρείτε την εφαρμογή στο <a href='%LINK2%' target='_blank'>φάκελο περιεχομένου του ArcGIS Online</a>.",
+				firstSaveQ2: "Η εφαρμογή μου κοινοποιείται;",
+				firstSaveA2: "Προς το παρόν η εφαρμογή σας δεν κοινοποιείται. Για να την κοινοποιήσετε, χρησιμοποιήστε το κουμπί ΚΟΙΝΟΠΟΙΗΣΗ.",
+				shareTitle: "Κοινοποίηση της εφαρμογής σας",
+				sharePrivateHeader: "Η εφαρμογή σας δεν κοινοποιείται, θέλετε να την κοινοποιήσετε;",
+				sharePrivateBtn1: "Κοινοποίηση δημοσίως",
+				sharePrivateBtn2: "Κοινοποίηση στον οργανισμό μου",
+				sharePrivateProgress: "Κοινοποίηση σε εξέλιξη...",
+				sharePrivateErr: "Η κοινοποίηση απέτυχε, δοκιμάστε ξανά ή",
+				sharePrivateOk: "Η κοινοποίηση ενημερώθηκε με επιτυχία, φόρτωση...",
+				shareStatus1: "Η εφαρμογή δεν έχει αποθηκευτεί",
+				shareStatus2: "Η εφαρμογή κοινοποιείται δημοσίως",
+				shareStatus3: "Η εφαρμογή κοινοποιείται εντός του οργανισμού",
+				shareStatus4: "Η εφαρμογή δεν κοινοποιείται",
+				sharePreviewAsUser: "Προεπισκόπηση",
+				shareHeader1: "Η εφαρμογή σας είναι <strong>προσβάσιμη δημοσίως</strong>.",
+				shareHeader2: "Η εφαρμογή σας είναι προσβάσιμη από τα μέλη του οργανισμού σας (απαιτείται σύνδεση).",
+				shareLinkHeader: "Κοινοποιήστε την εφαρμογή στο κοινό σας",
+				shareLinkOpen: "ΑΝΟΙΓΜΑ",
+				learnMore: "Μάθετε περισσότερα",
+				shareQ1Opt1: "Πώς μπορώ να διατηρήσω ιδιωτική την εφαρμογή;",
+				shareQ1Opt2: "Πώς μπορώ να διατηρήσω ιδιωτική την εφαρμογή ή να την κοινοποιήσω δημοσίως;",
+				shareA1: "Χρησιμοποιήστε την επιλογή %SHAREIMG% στη <a href='%LINK1%' target='_blank'>σελίδα στοιχείων της εφαρμογής</a>. Εάν θέλετε επίσης να καταργήσετε την κοινοποίηση του web χάρτη, χρησιμοποιήστε τη <a href='%LINK2%' target='_blank'>σελίδα στοιχείων του web χάρτη</a>.",
+				shareA1bis: "Εάν θέλετε επίσης να καταργήσετετην κοινοποίηση του Feature service, χρησιμοποιήστε τη <a href='%LINK1%' target='_blank'>σελίδα στοιχείων του Feature service</a>.",
+				shareQ2: "Πώς μπορώ να επεξεργαστώ την εφαρμογή αργότερα;",
+				shareQ2bis: "Πώς μπορώ να επιστρέψω στο περιβάλλον διαμόρφωσης;",
+				shareA2div1: "Αποθηκεύστε και χρησιμοποιήστε ξανά το σύνδεσμο %LINK1% ή χρησιμοποιήστε τη <a href='%LINK2%' target='_blank'>σελίδα στοιχείων της εφαρμογής</a>.",
+				shareA2div2: "Ως κάτοχος της εφαρμογής, όταν είστε συνδεδεμένοι στο ArcGIS.com, η εφαρμογή περιλαμβάνει ένα κουμπί για άνοιγμα του διαδραστικού εργαλείου δημιουργίας:",				
+				shareQ3: "Πού αποθηκεύονται τα δεδομένα;",
+				shareA3: "Η διαμόρφωση της εφαρμογής αποθηκεύεται σε αυτό το web αντικείμενο εφαρμογής </a>.",
+				shareWarning: "Η κοινοποίηση %WITH% έχει απενεργοποιηθεί επειδή δεν είστε ο κάτοχος του <a href='%LINK%' target='_blank'>web χάρτη</a>.",
+ 				shareWarningWith1: "δημοσίως",
+ 				shareWarningWith2: "δημοσίως και στον Oργανισμό"
+			},
+			directCreation: {
+				header: "Καλώς ορίσατε στο εργαλείο δημιουργίας  Swipe/Spyglass",
+				mapPickHeader: "Για να ξεκινήσετε, εισαγάγετε ένα έγκυρο ID web χάρτη ή χρησιμοποιήστε το κουμπί αναζήτησης για να αναζητήσετε web χάρτες.",
+				launchBuilder: "Εκκίνηση του εργαλείου δημιουργίας",
+				chooseWebmapLbl: "Επιλέξτε web χάρτη...",
+				explain2: "Για να δημιουργήσετε ένα Swipe ή Spyglass story map, χρησιμοποιήστε το παρακάτω κουμπί για να επιλέξετε τον υπάρχοντα web χάρτη του ArcGIS Online που θέλετε να χρησιμοποιήσετε. Εναλλακτικά, μπορείτε να επικολλήσετε το ID του web χάρτη στο παρακάτω πεδίο.",
+				explain3: "Εάν θέλετε να χρησιμοποιήσετε δύο web χάρτες στο story map σας, θα σας ζητηθεί ο δεύτερος web χάρτης αργότερα όταν ορίσετε αυτή την επιλογή.",
+				webmapPlaceholder: "Εισαγάγετε ένα ID web χάρτη..."
+			}
+		},
+		configure: {
+			mapdlg:{
+				items:{
+					organizationLabel: "Ο Οργανισμός μου",
+					onlineLabel: "ArcGIS Online",
+					contentLabel: "Το Περιεχόμενό μου",
+					favoritesLabel: "Τα Αγαπημένα μου"
+				},
+				title: "Επιλογή web χάρτη",
+				searchTitle: "Αναζήτηση",
+				ok: "OK",
+				cancel: "Άκυρο",
+				placeholder: "Εισαγάγετε όρο αναζήτησης"
+			}
+		}
+    })
+);
+
